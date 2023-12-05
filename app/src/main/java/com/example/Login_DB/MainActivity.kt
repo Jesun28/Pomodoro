21323e77649b18f4a8f32aa046be47c0b7e7a1bb
Jesun28 <100650220+Jesun28@users.noreply.github.com>
2023-12-05 17:54:41 +0800

    1231

--- app/src/main/java/com/example/Login_DB/MainActivity.kt
+++ app/src/main/java/com/example/Login_DB/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.Login_DB
-// jerson
+
 import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +9,7 @@ import com.example.Login_DB.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-//123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
